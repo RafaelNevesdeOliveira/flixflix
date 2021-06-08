dbe27a203dfab76be953d9021b9fd95be621f137
Rafael Neves <rafaelneves652@gmail.com>
2021-06-08 01:21:38 -0300

    added series crude

--- src/app/pages/api-tests/api-tests.component.ts
+++ src/app/pages/api-tests/api-tests.component.ts
@@ -20,10 +20,34 @@ export class ApiTestsComponent implements OnInit {
     this.getAnimes()
   }
 
+  saveAnimes(form : NgForm){
+    if(this.anime._id !== undefined){
+      this.apiService.updateAnimes(this.anime).subscribe(()=> {
+        this.cleanForm(form);
+      })
+    }
+  }
+
   getAnimes(){
     this.apiService.getAnimes().subscribe((animes:Animes[]) =>{
       this.animes = animes;
     })
+  }
+
+  deleteAnimes(anime:Animes){
+    this.apiService.deleteAnimes(anime).subscribe(()=>{
+      this.getAnimes();
+    })
+  }
+
+  editAnimes(anime: Animes){
+    this.anime = {...anime}
+  }
+
+  cleanForm(form: NgForm){
+    this.getAnimes();
+    form.resetForm();
+    this.anime = {} as Animes;
   }
 
 }
--- src/app/pages/api-tests/services/api-test.service.ts
+++ src/app/pages/api-tests/services/api-test.service.ts
@@ -17,8 +17,69 @@ export class ApiTestService {
 
   constructor(private httpClient: HttpClient) {}
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
 
+  /**
+   *
+   * @Type Obtem toda a lista
+   */
   getAnimes(): Observable<Animes[]> {
-    return this.httpClient.get<Animes>(this.API_URL).pipe(map(res => res.result));
+    return this.httpClient
+      .get<Animes>(this.API_URL)
+      .pipe(map((res) => res.result));
   }
+
+  getAnimesById(_id: number): Observable<Animes> {
+    return this.httpClient.get<Animes>(this.API_URL + '/' + _id).pipe(
+      retry(2),
+      catchError(this.handleError)
+    );
+  }
+
+  saveAnimes(anime: Animes): Observable<Animes> {
+    return this.httpClient
+      .post<Animes>(this.API_URL, JSON.stringify(anime), this.httpOptions)
+      .pipe(
+        retry(2),
+        catchError(this.handleError)
+      );
+  }
+
+  updateAnimes(anime: Animes): Observable<Animes> {
+    return this.httpClient
+      .put<Animes>(
+        this.API_URL + '/' + anime._id,
+        JSON.stringify(anime),
+        this.httpOptions
+      )
+      .pipe(
+        retry(1),
+        catchError(this.handleError)
+      );
+  }
+
+  deleteAnimes(anime: Animes) {
+    return this.httpClient
+      .delete<Animes>(this.API_URL + '/' + anime._id, this.httpOptions)
+      .pipe(
+        map((res) => res.result),
+        retry(1),
+        catchError(this.handleError)
+      );
+  }
+
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if(error.error instanceof ErrorEvent){
+      errorMessage = error.error.message;
+    }else{
+      errorMessage = `Código do erro: ${error.status},` + `mensagem: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage)
+  }
+
+
 }
